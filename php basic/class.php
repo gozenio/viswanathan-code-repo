34572ec8bef8ec8df984df787a343fc59920ad74
viswasree20 <99312266+viswasree20@users.noreply.github.com>
2022-02-16 18:17:18 +0530

    Update class.php

--- php basic/class.php
+++ php basic/class.php
@@ -1,12 +1,70 @@
 <?php
+	//basic class object
 	class Fruit{
 		public $name;
-		function _constructor($name){
+		public $color;
+		public function __construct($name,$color){
 			$this->name=$name;
+			$this->color=$color;
+			
 		}
-		function _destructor(){
-			echo $this->name;
+		// function __destructor(){
+		// 	echo $this->name;
+		// }
+		public function intro(){
+			echo "The fruit is {$this->name} and the color is {$this->color}.";
 		}
 	}
-	$apple=new Fruit('Apple');
+	//$apple=new Fruit('Apple');
+
+	//public,protected,private
+	/**
+	 * 
+	 */
+	// class Fruit	{
+	// 	public $name;
+	// 	public $color;
+	// 	public $weight;
+	// 	function set_get($name)
+	// 	{
+	// 		$this->name=$name;
+	// 	}
+	// 	protected function set_color($name)
+	// 	{
+	// 		$this->name=$name;
+	// 	}
+	// 	private function set_weigth($name)
+	// 	{
+	// 		$this->name=$name;
+	// 	}
+	// }
+
+
+
+	Class Berry extends Fruit{
+		public $weight;
+		public function __construct($name,$color,$weight){
+			$this->name=$name;
+			$this->color=$color;
+			$this->weight=$weight;
+
+		}
+		function __destruct(){
+			echo "{$this->name}:{$this->color}:{$this->weight}";
+		}
+		// public function message(){
+		// // 	echo 'this is Fruit or berry';
+		// echo "{$this->name}:{$this->color}:{$this->weight}";
+		// }
+	}
+	// $strawberry=new Berry("strawberry","red");
+	// $strawberry->message();
+	// $strawberry->intro();
+
+	$apple =new Fruit("Apple","color","300");
+	//$apple->message();
+	// $apple->set_get('Apple');
+	// $apple->set_color('red');
+	// $apple->set_weigth(300);
+
   ?>
