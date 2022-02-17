87473502d3b7d6908845b5766616f932082bacb2
viswasree20 <99312266+viswasree20@users.noreply.github.com>
2022-02-17 18:09:53 +0530

    Update sql.php

--- php basic/sql.php
+++ php basic/sql.php
@@ -32,13 +32,90 @@
 	// }
 
 		//multi_insert data;
-	$sql="INSERT INTO myGuests(firstname,lastname,email)VALUES('VISWANATHAN','R','viswasree20@gamil.com');";
-	$sql.="INSERT INTO myGuests(firstname,lastname,email)VALUES('vicky','m','vick26@gamil.com');";
-	$sql.="INSERT INTO myGuests(firstname,lastname,email)VALUES('jegan','p','jegan12@gamil.com');";
-	if($db->multi_query($sql)===TRUE){
-		//$last_id=$db->insert_id;
-	 	echo "inserted data";
+	// $sql="INSERT INTO myGuests(firstname,lastname,email)VALUES('VISWANATHAN','R','viswasree20@gamil.com');";
+	// $sql.="INSERT INTO myGuests(firstname,lastname,email)VALUES('vicky','m','vick26@gamil.com');";
+	// $sql.="INSERT INTO myGuests(firstname,lastname,email)VALUES('jegan','p','jegan12@gamil.com');";
+	// if($db->multi_query($sql)===TRUE){
+	// 	//$last_id=$db->insert_id;
+	//  	echo "inserted data";
+	// }else{
+	//  	echo "error :".$db.error();
+	//}
+
+		//prepare
+// 	$sql=$db->prepare('INSERT INTO myguests(firstname,lastname,email)VALUES(?,?,?)');
+// 	$sql->bind_param("sss",$firstname,$lastname,$email);
+
+// 	//set parameter
+// 	$firstname="selva";
+// 	$lastname="Ganesh";
+// 	$email="aswin09@gmail.com";
+// 	$sql->execute();
+// 	$sql->close();
+// 	$db->close();
+
+		//select data
+	// $sql="SELECT id,firstname,lastname,email FROM myguests";
+	// $result=$db->query($sql);
+	// if($result->num_rows>0){
+	// 	while($row=$result->fetch_assoc()){
+	// 		echo"id: ".$row["id"]."-name: ".$row["firstname"]."<br>";
+	// 	}
+	// } else{
+	// 	echo "0 result";
+	// }
+
+		//filter with "where";
+	// $sql="SELECT id,firstname,lastname FROM myguests WHERE lastname='ganesh'";
+	// $result=$db->query($sql);
+	// if($result->num_rows>0){
+	// 	while($row=$result->fetch_assoc()){
+	// 		echo"id: ".$row["id"]."-name: ".$row["firstname"]."<br>";	
+	// 	}
+	// }else{
+	// 	echo"result 0";
+	// }
+	// $db->close();
+
+		//order
+	// $sql="SELECT id,firstname,lastname FROM myguests ORDER BY firstname";
+	// $result=$db->query($sql);
+	// if($result->num_rows>0){
+	// 	while($row=$result->fetch_assoc()){
+	// 		echo"id: ".$row["id"]."-name: ".$row["firstname"]."<br>";
+	// 	}
+	// }else{
+	// 	echo"result 0";
+	// }
+
+		//delete
+	// $sql="DELETE FROM myguests WHERE lastname='R'";
+	// if($db->query($sql)===TRUE){
+	// 	echo"record delete";
+	// }
+	// else{
+	// 	echo "error";
+	// }
+
+	// $db->close();
+
+		//update
+	// $sql ="UPDATE myguests SET lastname='ganeshan'WHERE firstname='selva'";
+	// if($db->query($sql)===true){
+	// 	echo "data updated!";
+	// }
+	// else{
+	// 	echo "error";
+	// }
+
+	$sql="SELECT * FROM myguests LIMIT 1,2";
+	$result=$db->query($sql);
+	if($result->num_rows>0){
+		while($row=$result->fetch_assoc()){
+			echo"id: ".$row["id"]."-name: ".$row["firstname"]."<br>";
+		}
 	}else{
-	 	echo "error :".$db.error();
+		echo"result 0";
 	}
-?>
+	$db->close();
+ ?>
